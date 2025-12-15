2b0990403b27ba24d69687e582235ee255bd7f8b
Maxiee <maxieewong@gmail.com>
2025-12-16 00:04:52 +0800

    feat: 更新 HomeView，添加基本布局并创建 repo_to_prompt 视图和绑定文件

--- apps/maeiee_system_toolkit/lib/views/home/home_view.dart
+++ apps/maeiee_system_toolkit/lib/views/home/home_view.dart
@@ -5,6 +5,10 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SingleChildScrollView(child: Column(children: [
+        
+      ],)),
+    );
   }
 }
