25dc4b261ab23cf61bbb3351b2e29ae9afa93d99
Maxiee <maxieewong@gmail.com>
2025-12-21 14:13:27 +0800

    feat: 添加项目配置文件，更新 Gradle 设置，集成 FlutterBoost 依赖，支持 Flutter 模块和状态管理

---
+++ demos/flutter_boost/FlutterBoostDemo/app/src/main/java/com/maxiee/flutterboost/demo/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.maxiee.flutterboost.demo
+
+import android.app.Application
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        
+    }
+}
